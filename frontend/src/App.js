10271789c28dbd8bbc17a85ca47a541714f87e4d
Rahul1729github <prajapatirahul1729@gmail.com>
2021-08-20 19:54:11 +0530

    up to video 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
-
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
-    <h1>welcome to proshop</h1>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+        <h1>welcome to proshop</h1>
+      </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Nav, Navbar, Container } from 'react-bootstrap'
+const Header = () => {
+  return (
+    <header>
+     
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='/'>Ecommerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>              
+            </Nav>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              <Nav.Link href='/login'><i className='fas fa-user'></i>Sign in</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
