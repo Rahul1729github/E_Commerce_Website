d2182396a1ed65456cb6e8ff554038b1df49e21b
Rahul1729github <prajapatirahul1729@gmail.com>
2021-08-31 18:57:42 +0530

    2 frontend added routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Nav, Navbar, Container } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 const Header = () => {
   return (
     <header>
      
       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <Container>
-          <Navbar.Brand href='/'>Ecommerce</Navbar.Brand>
+          <LinkContainer to='/'>
+          <Navbar.Brand >Ecommerce</Navbar.Brand>
+          </LinkContainer>
+            
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='me-auto'>              
             </Nav>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-              <Nav.Link href='/login'><i className='fas fa-user'></i>Sign in</Nav.Link>
+              <LinkContainer to='/cart'>
+              <Nav.Link ><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+              <Nav.Link ><i className='fas fa-user'></i>Sign in</Nav.Link>
+              </LinkContainer>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
